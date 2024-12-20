133282bba03eb945b4d16d8270da788df4d19bf2
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-12-20 23:51:23 +0100

    Email from server

--- WebApi/Services/EmailSender.cs
+++ WebApi/Services/EmailSender.cs
@@ -1,6 +1,24 @@
-﻿namespace WebApi.Services
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace WebApi.Services
 {
-    public class EmailSender
+    public class EmailSender : IEmailSender
     {
+        public Task SendEmailAsync(string email, string subject, string message) 
+        {
+            var mail = "RealEmailAdress@outlook.com";
+            var pw = "Password123";
+
+            var client = new SmtpClient()
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            return client.SendMailAsync(
+                new MailMesseage()
+                );
+        }
     }
 }
