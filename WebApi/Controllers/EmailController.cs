fc15a55b40f9bfdd8255a9c17ffb0a59f93f07db
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2025-01-07 22:59:59 +0100

    Update EmailController.cs

--- WebApi/Controllers/EmailController.cs
+++ WebApi/Controllers/EmailController.cs
@@ -1,6 +1,34 @@
-﻿namespace WebApi.Controllers
+﻿using DataLayer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi.DTOs;
+using WebApi.Services;
+using Mapster;
+
+namespace WebApi.Controllers
 {
-    public class EmailController
+
+    [ApiController]
+    [Route("api/email")]
+    [EnableCors("AllowReactApp")]
+    public class EmailController : Controller
     {
+        private readonly IDataService _dataService;
+        private readonly IConfiguration _configuration;
+        private readonly IEmailSender _emailSender;
+
+        public EmailController(IDataService dataService, IConfiguration configuration, IEmailSender emailSender)
+        {
+            _configuration = configuration;
+            _dataService = dataService;
+            _emailSender = emailSender;
+        }
+
+
     }
 }
